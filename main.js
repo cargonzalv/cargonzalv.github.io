094dd27391d9ad4d499466f737b033fffb5ecd19
Carlos Gonzalez <ce.gonzalez13@uniandes.edu.co>
2017-08-30 08:03:57 -0500

    done

--- main.js
+++ main.js
@@ -32,7 +32,7 @@
     if (elems.hasClass('active'))
       return;
 
-    if (isElementInViewport(elems)) {
+    if (isElementInViewport(2751, 2815)) {
           // Start the animation
           elems.each(function(index){
             $(this).delay(index*200).queue(function(nxt) {
@@ -44,19 +44,261 @@
       });
 
 
-  function isElementInViewport(elem) {
-    var $elem = $(elem);
+  function isElementInViewport(top, bot) {
 
       // Get the scroll position of the page.
       var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
       var viewportTop = $(scrollElem).scrollTop();
       var viewportBottom = viewportTop + $(window).height();
 
-      // Get the position of the element on the page.
-      var elemTop = Math.round( $elem.offset().top );
-      var elemBottom = elemTop + $elem.height();
+      
 
-      return ((1338 < viewportBottom) && (1438 > viewportTop));
+      return ((top < viewportBottom) && (bot > viewportTop));
     }
 
 
+/*
+* Flux 3D Carousel
+* Author: Dean Coulter
+* Licensed under the MIT license
+* 
+* Version 0.1
+*/
+
+$.fn.carousel3d = function(args){
+
+  var el = ({
+    carousel_frame: $(this)
+
+  });
+
+  var size = el.carousel_frame.children().length; 
+  var panelSize = el.carousel_frame.width();
+  var translateZ = Math.round( ( panelSize / 2 ) / Math.tan( Math.PI / size ) );
+
+  el.carousel_frame.css({
+    "transform": "rotateY(0deg) translateZ(-"+translateZ+"px)"
+  })
+
+  var rotateY = 0;
+  var rotate_int = 0;
+  var ry =  360/size;
+  var box = 0;
+
+  function animate_slider(index){
+    rotateY = ry*rotate_int;
+    $("#test").text(rotateY+", "+rotate_int+", ");
+    
+    for(var i=0;i<size;i++){
+      var z = (rotate_int*-1*ry)+(i*ry);
+      if(i != index){
+        el.carousel_frame.children("figure:eq("+i+")").css({
+          "transform":"rotateY("+z+"deg ) translateZ("+translateZ+"px)",
+          "color": "transparent",
+          "text-shadow": "0 0 5px rgba(0,0,0,0.8)"
+        });
+      }
+      else{
+       el.carousel_frame.children("figure:eq("+i+")").css({
+        "transform":"rotateY("+z+"deg ) translateZ("+translateZ+"px)",
+        "color":"white"
+      });
+     }
+   }
+   rotateY = 0;
+    box = 0; // reset rotateY, ready for re-use
+  }
+
+  animate_slider(0);
+
+
+
+  el.carousel_frame.children().on("click", function(){
+    var new_int = $(this).index();
+    var dir = (rotate_int%size!=0 || new_int!=(size-1)) && (((rotate_int%size < new_int) && rotate_int >=0) || (-rotate_int%size >= new_int && rotate_int < 0) || (rotate_int%size==(size-1) && new_int == 0));
+    if(dir){
+      rotate_int++;
+    } 
+    else if(!dir){
+      rotate_int--;
+    } 
+    animate_slider(new_int);
+  }); 
+};
+
+$(document).ready(function(){
+  $('#carousel').carousel3d();
+});
+$.fn.extend({     
+
+  percentcircle :  function(options) {
+    //Set the default values, use comma to separate the settings, example:
+    var defaults = {
+      animate : true,
+      diameter : 150,
+      guage: 5,
+      coverBg: '#fff',
+      bgColor: '#efefef',
+      fillColor: '#5c93c8',
+      percentSize: '15px',
+      percentWeight: 'normal'
+    },
+    styles = {
+      cirContainer : {
+        'width':defaults.diameter,
+        'height':defaults.diameter
+      },
+      cir : {
+        'position': 'relative',
+        'text-align': 'center',
+        "margin-left":"50%",
+        'width': defaults.diameter,
+        'height': defaults.diameter,
+        'border-radius': '100%',
+        'background-color': defaults.bgColor,
+        'background-image' : 'linear-gradient(91deg, transparent 50%, '+defaults.bgColor+' 50%), linear-gradient(90deg, '+defaults.bgColor+' 50%, transparent 50%)'
+      },
+      cirCover: {
+        'position': 'relative',
+        'top': defaults.guage,
+        'left': defaults.guage,
+        'text-align': 'center',
+        'width': defaults.diameter - (defaults.guage * 2),
+        'height': defaults.diameter - (defaults.guage * 2),
+        'border-radius': '100%',
+        'background-color': defaults.coverBg
+      },
+      percent: {
+        'display':'block',
+        'width': defaults.diameter,
+        'height': defaults.diameter,
+        'line-height': defaults.diameter + 'px',
+        'vertical-align': 'middle',
+        'font-size': defaults.percentSize,
+        'font-weight': defaults.percentWeight,
+        'color': defaults.fillColor
+      },
+      content: {
+        "horizontal-align":"middle",
+        "margin-left":"1500px"
+      }
+    };
+
+    var that = this,
+    template = '<div><div class="ab"><div class="cir"><span class="perc">{{percentage}}</span></div></div></div><div class="cont" style="margin-left:27%;font-size:25px">{{content}}</div>',          
+    options =  $.extend(defaults, options)          
+
+    function init(){
+
+      that.each(function(){
+        var elemTop = Math.round( $(this).offset().top );
+        var elemBottom = elemTop + $(this).height();
+        console.log(elemBottom, elemTop);
+        var $this = $(this),
+              //we need to check for a percent otherwise set to 0;
+            perc = Math.round($this.data('percent')), //get the percentage from the element
+            cont = $this.data("content"),
+            deg = perc * 3.6,
+            stop = options.animate ? 0 : deg,
+            $chart = $(template.replace('{{percentage}}',perc+'%').replace("{{content}}",cont));
+            //set all of the css properties forthe chart
+            $chart.css(styles.cirContainer).find('.ab').css(styles.cir).find('.cir').css(styles.cirCover).find('.perc').css(styles.percent).find(".cont").css(styles.content);
+
+          $this.append($chart); //add the chart back to the target element
+          let animated;
+          $(window).scroll(function(){
+            if(isElementInViewport(1759,2359) && !animated){  
+            animateChart(deg,parseInt(stop),$chart.find('.ab')); //both values set to the same value to keep the function from looping and animating 
+            animated = true; 
+          }
+        });
+        });
+    }
+
+    var animateChart = function (stop,curr,$elm){
+      var deg = curr;
+      if(curr <= stop){
+        if (deg>=180){
+          $elm.css('background-image','linear-gradient(' + (90+deg) + 'deg, transparent 50%, '+options.fillColor+' 50%),linear-gradient(90deg, '+options.fillColor+' 50%, transparent 50%)');
+        }else{
+          $elm.css('background-image','linear-gradient(' + (deg-90) + 'deg, transparent 50%, '+options.bgColor+' 50%),linear-gradient(90deg, '+options.fillColor+' 50%, transparent 50%)');
+        }
+        curr ++;
+        setTimeout(function(){
+          animateChart(stop,curr,$elm);
+        },1);
+      }
+    };  
+    init(); 
+  }
+});
+$('.demo-1').percentcircle();
+
+$('.demo-2').percentcircle({
+  animate : true,
+  diameter : 200,
+  guage: 3,
+  coverBg: '#fff',
+  bgColor: '#efefef',
+  fillColor: '#E95546',
+  percentSize: '15px',
+  percentWeight: 'normal'
+});
+
+$('.demo-3').percentcircle({
+  animate : false,
+  diameter : 200,
+  guage: 3,
+  coverBg: '#fff',
+  bgColor: '#efefef',
+  fillColor: '#DA4453',
+  percentSize: '18px',
+  percentWeight: 'normal'
+});
+$('.demo-4').percentcircle({
+  animate : true,
+  diameter : 200,
+  guage: 3,
+  coverBg: '#fff',
+  bgColor: '#efefef',
+  fillColor: '#46CFB0',
+  percentSize: '18px',
+  percentWeight: 'normal'
+});   
+$('.demo-5').percentcircle({
+  animate : true,
+  diameter : 200,
+  guage: 3,
+  coverBg: '#fff',
+  bgColor: '#efefef',
+  fillColor: '#8BC163',
+  percentSize: '18px',
+  percentWeight: '20px'
+}); 
+$('.demo-6').percentcircle({
+  animate : true,
+  diameter : 200,
+  guage: 10,
+  coverBg: '#fff',
+  bgColor: '#efefef',
+  fillColor: '#D870A9',
+  percentSize: '18px',
+  percentWeight: 'normal'
+});   
+
+$(document).ready(function() {
+  var facts = [
+  "Click on my head to learn more about me!"
+  ,"I was born the 18th of January..."
+  ,"at 6:08 pm..."
+  ,"in Caracas, Venezuela..."
+  ,"my parents had been married for 9 years."
+  ],
+  pickANumber = 1;
+
+  jQuery("#pogueHead").on("click", function() {
+    if (pickANumber == facts.length) { pickANumber = 0; }
+    jQuery(".factText").text(facts[pickANumber]);
+    pickANumber++;
+  });
+});
